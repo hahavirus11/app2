6836ab73f146ef86ea28eefe781c3ef70b04cab8
Bivash Rajbanshi <bibash_banshi@yahoo.com>
2018-11-18 14:35:40 +0545

    first commit

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.get('/data', function(req, res, next) {
+    res.send('this is another sub page of users.js');
+});
+
+module.exports = router;
